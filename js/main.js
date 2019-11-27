738ee02392f4adfdcfb994e0d4479900ef241655
Abdesslame Abouibrahim <aabouibr@e1r6p10.1337.ma>
2019-11-27 18:30:54 +0100

    adding audio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var video = document.querySelector("#videoElement");
+//var audio = new Audio('../audio_file.mp3');
+//audio.play();
 
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
