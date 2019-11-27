3587012582c3ffe1e04d9dd513d407e9ee43739d
Abdesslame Abouibrahim <aabouibr@e1r6p10.1337.ma>
2019-11-27 18:44:43 +0100

    suck my a** google

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var video = document.querySelector("#videoElement");
 var audio = new Audio('audio_file.mp3');
-audio.play();
-audio.loop = true;
 
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
+      audio.play();
+      audio.loop = true;
       video.srcObject = stream;
     })
     .catch(function (err0r) {
