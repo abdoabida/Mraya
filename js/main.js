f399bcf1cdcb55c5b0c404ddaf53b91ef6b2ab84
Abdesslame Abouibrahim <aabouibr@e1r6p10.1337.ma>
2019-11-27 16:21:37 +0100

    removed assets folder and console log media stream

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
       video.srcObject = stream;
-      console.log(video.srcObject)
     })
     .catch(function (err0r) {
       console.log("Something went wrong!");
